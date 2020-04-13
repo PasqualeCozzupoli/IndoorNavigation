7cc8b7f7f0bf3d3c4c07c7536d66264d283eb658
pasqualecozzupoli <pasqualecozzupoli@imacdipasquale.homenet.telecomitalia.it>
2020-04-13 19:41:54 +0200

    First commit

---
+++ IndoorNavigation/indoorNavigation-frontend/src/test/java/it/indoor/navigation/frontend/ApplicationTests.java
@@ -0,0 +1,19 @@
+package it.indoor.navigation.frontend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Unit test for simple App.
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+}
